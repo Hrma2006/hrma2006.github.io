78c6a0e0239dacb5f2a192d6a02159b1a1250c8c
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-10-11 16:11:16 +0300

    deleted unnecessary lines

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-window.addEventListener('load',()=>{
+const button = document.getElementById("button");
+const body = document.querySelector("body");
+button.addEventListener("click", () => {
+	if (button.innerText == "اضغط لتفعيل الوضع المظلم") {
+		button.innerText = "اضغط لتفعيل الوضع المضيئ";
+		button.style.backgroundColor = "white";
+		button.style.color = "black";
+		body.style.backgroundColor = "black";
+	} else {
+		button.innerText = "اضغط لتفعيل الوضع المظلم";
 
-    const button=document.getElementById('button');
-    const body=document.querySelector('body');
-    button.addEventListener('click',()=>{
-        if(button.innerText=='اضغط لتفعيل الوضع المظلم'){
-            button.innerText='اضغط لتفعيل الوضع المضيئ'
-            button.style.backgroundColor='white'
-            button.style.color='black'
-            body.style.backgroundColor='black'
-            
-        }
-        else {button.innerText='اضغط لتفعيل الوضع المظلم'
+		button.style.color = "white";
+		button.style.backgroundColor = "black";
+		body.style.backgroundColor = "white";
+	}
+});
 
-            button.style.color='white'
-            button.style.backgroundColor='black'
-            body.style.backgroundColor='white'
-
-           }
-    ;})
-})
 // done
